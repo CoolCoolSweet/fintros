fc9c3a8024c94012b037ac4f61f608c88604b39b
Sean <38589300+CoolCoolSweet@users.noreply.github.com>
2018-07-30 23:49:02 -0400

    Static Articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'articles#index'
+  get 'articles/post1' => 'articles#post1'
+  get 'articles/post2' => 'articles#post2'
+  get 'articles/post3' => 'articles#post3'
+  get 'articles/post4' => 'articles#post4'
 
   resources :articles
 end
